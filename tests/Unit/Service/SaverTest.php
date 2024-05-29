7b836bb1557d41a776a024574ed61513df94e763
Anatoliy Melnikov <5785276@gmail.com>
2024-05-29 23:19:01 +0300

    Test Saver

---
+++ tests/Unit/Service/SaverTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Aeliot\PhpCsFixerBaseline\Test\Unit\Service;
+
+use Aeliot\PhpCsFixerBaseline\Model\BaselineFile;
+use Aeliot\PhpCsFixerBaseline\Service\Reader;
+use Aeliot\PhpCsFixerBaseline\Service\Saver;
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\TestCase;
+
+#[CoversClass(Saver::class)]
+final class SaverTest extends TestCase
+{
+    public function testSave(): void
+    {
+        $pathTMP = sys_get_temp_dir().'/pcsf-baseline-'.date('YmdHis').'-'.random_int(0, 9999).'.json';
+        $path = __DIR__.'/../../fixtures/.php-cs-fixer-baseline.json';
+
+        $baselineFile = $this->createMock(BaselineFile::class);
+        $baselineFile->method('getPath')->willReturn($pathTMP);
+        $baselineFile->method('getContent')->willReturn((new Reader())->read($path)->getContent());
+
+        (new Saver())->save($baselineFile);
+
+        self::assertFileEquals($path, $pathTMP);
+
+        unlink($pathTMP);
+    }
+}
