b2035a2fc3ff7a06f887e6dbffa3c605153ce389
Anatoliy Melnikov <5785276@gmail.com>
2024-08-31 00:02:58 +0300

    Fix code stile

--- scripts/box/prepare-config.php
+++ scripts/box/prepare-config.php
@@ -2,8 +2,17 @@
 
 declare(strict_types=1);
 
+/*
+ * This file is part of the box project.
+ *
+ * (c) Anatoliy Melnikov <5785276@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
 $configPath = __DIR__ . '/config.json';
-$config = json_decode(file_get_contents($configPath), true, 512, JSON_THROW_ON_ERROR);
+$config = json_decode(file_get_contents($configPath), true, 512, \JSON_THROW_ON_ERROR);
 
 $config['base-path'] = dirname($configPath, 2);
 
