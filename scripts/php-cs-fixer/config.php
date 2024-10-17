43486928432c8c9c1ee6fa8ad613f02c374ad153
Anatoliy Melnikov <5785276@gmail.com>
2024-10-17 23:44:20 +0300

    Fix code style

--- scripts/php-cs-fixer/config.php
+++ scripts/php-cs-fixer/config.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the PHP CS Fixer Baseline project.
+ *
+ * (c) Anatoliy Melnikov <5785276@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
 use Aeliot\PhpCsFixerBaseline\Service\FilterFactory;
 
 $rules = [
--- scripts/php-cs-fixer/finder.php
+++ scripts/php-cs-fixer/finder.php
@@ -2,6 +2,15 @@
 
 declare(strict_types=1);
 
+/*
+ * This file is part of the PHP CS Fixer Baseline project.
+ *
+ * (c) Anatoliy Melnikov <5785276@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
 return $finder = (new PhpCsFixer\Finder())
     ->files()
     ->in(dirname(__DIR__, 2))
@@ -9,6 +18,3 @@ return $finder = (new PhpCsFixer\Finder())
     ->append([
         'bin/pcsf-baseline',
     ]);
-
-
-
